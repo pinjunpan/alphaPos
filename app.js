dac071fd52713b069a5f48ec8356822b133c5438
pinjunpan <pinjunpann@gmail.com>
2024-12-26 16:36:48 +0800

    Add Drink constructor function

---
+++ app.js
@@ -0,0 +1,7 @@
+function Drink (name, sugar, ice) {
+  this.name = name
+  this.sugar = sugar
+  this.ice = ice
+}
+let bubbleMilkTea = new Drink('Bubble Milk Tea', 'No Sugar', 'Less Ice')
+console.log(bubbleMilkTea)
