c6c03212a264228b5c998c964d79523d6383636e
pinjunpan <pinjunpann@gmail.com>
2024-12-27 16:39:10 +0800

    Add event listener to 'orderlists'

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ addDrinkButton.addEventListener('click', function () {
   alphaPos.addDrink(drink)
 })
 
+const orderLists = document.querySelector('[data-order-lists]')
+orderLists.addEventListener('click', function(event) {
+  console.log(event.target)
+})
+
 // Constructor function for Alpha Pos System
 function AlphaPos () { }
 
@@ -31,7 +36,6 @@ AlphaPos.prototype.getCheckedValue = function (inputName) {
   return selectedOption
 }
 
-const orderLists = document.querySelector('[data-order-lists]')
 AlphaPos.prototype.addDrink = function (drink) {
   let orderListsCard = `
     <div class="card mb-3">
